934e23ed095210923833292467b001242837e318
bianxd <WQ@DESKTOP-J8BU1RV>
2015-11-26 23:48:49 +0800

    加入了微博降维并融合后的数据

--- src/tree/main.py
+++ src/tree/main.py
@@ -7,19 +7,19 @@ import treepredict
 import data
 from DecisionTree import DecisionTree
 
-path = 'G://kdd cup 2012//KDD Cup Track 1 Data//track1//'
-train_file = path + 'train_1000.txt'
+path = '..//data//'
+train_file = path + 'weibo_train_data.txt'
 
 
 train_main = data.read_filedata(train_file,'ALL','|',[5,7,8])
 
-for row in train_main:
-    row.pop(0)
-    row.pop(0)
-    row.pop(1)
-    row.pop(1)
-    row.append(row[0])
-    row.pop(0)
+#for row in train_main:
+#    row.pop(0)
+#    row.pop(0)
+#    row.pop(1)
+#    row.pop(1)
+#    row.append(row[0])
+#    row.pop(0)
 
 #print train_main
  
